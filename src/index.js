9c16e97c068fb5c2a64c6b9235ebe118afffb146
Daiane Bolzan <dainha85@gmail.com>
2023-10-10 08:38:48 -0300

    FEAT: adiciona estrutura inicial do projeto

---
+++ src/index.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const express = require('express')
+const rotas = require('./rotas')
+
+const app = express()
+
+app.use(express.json())
+
+app.use(rotas)
+
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+	console.log(`Server running at http://localhost:${port}`)
+})
---
+++ src/rotas.js
@@ -0,0 +1,16 @@
+
+const express = require('express')
+
+const rotas = express()
+//TODO: fazer rotas
+// rotas.get('/categoria',)
+
+// rotas.post('/usuario',)
+// rotas.post('/login',)
+//* A partir daqui precisa verificação
+// rotas.use()
+
+// rotas.get('/usuario',)
+// rotas.put('/usuario',)
+
+module.exports = rotas
