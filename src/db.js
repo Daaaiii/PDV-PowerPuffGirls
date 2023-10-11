efde97dfdc4a4676e72e1df4f74814ead26667c0
Daiane Bolzan <dainha85@gmail.com>
2023-10-11 15:04:02 -0300

    FIX: correção de type na conexão com o banco

--- src/db.js
+++ src/db.js
@@ -6,7 +6,7 @@ const knex = require("knex")({
 		user: process.env.DB_USER,
 		password: process.env.DB_PASS,
 		database: process.env.DB,
-		ssl: {rejecUnauthorized: false},
+		ssl:{rejectUnauthorized: false},
 	},
 });
 module.exports = knex;
