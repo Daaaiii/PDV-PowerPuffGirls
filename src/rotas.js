cf20c7beadf879091bf7dcc098df69c6fed949f7
Bruna Fernandes <brunagfernandes@outlook.com.br>
2023-10-12 02:16:39 -0300

    Feat: Função Login Usuario

--- src/rotas.js
+++ src/rotas.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const usuarios = require('./controladores/usuarios');
+const { login } = require('./intermediarios/login');
 
 const rotas = express()
 //TODO: fazer rotas
 // rotas.get('/categoria',)
 
 rotas.post('/usuario', usuarios.cadastrarUsuario);
+rotas.post('/login', login)
 // rotas.post('/login',)
 //* A partir daqui precisa verificação
 // rotas.use()
