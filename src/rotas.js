1fb81db92604085d411c9ac4cb741153a386b810
Daiane Bolzan <dainha85@gmail.com>
2023-10-18 18:41:52 -0300

    CHORE: adiciona os arquivos para os controladorese e rotas de produto e cliente

--- src/rotas.js
+++ src/rotas.js
@@ -1,7 +1,11 @@
 const express = require("express");
-const { cadastrarUsuario, editarUsuario, detalharUsuario } = require("./controladores/usuarios");
-const { login } = require("./controladores/login");
-const { categoria } = require("./controladores/categoria");
+const {
+	cadastrarUsuario,
+	editarUsuario,
+	detalharUsuario,
+} = require("./controladores/usuarios");
+const {login} = require("./controladores/login");
+const {categoria} = require("./controladores/categoria");
 const verificaLogin = require("./intermediarios/verificaLogin");
 const validarRequisicao = require("./intermediarios/validaRequisicao");
 const usuarioSchema = require("./validacoes/usuario");
@@ -9,15 +13,25 @@ const loginSchema = require("./validacoes/login");
 
 const rotas = express();
 
-rotas.get('/categoria', categoria);
+rotas.get("/categoria", categoria);
 
 rotas.post("/usuario", validarRequisicao(usuarioSchema), cadastrarUsuario);
-rotas.post('/login', validarRequisicao(loginSchema), login)
+rotas.post("/login", validarRequisicao(loginSchema), login);
 
 rotas.use(verificaLogin);
 
-rotas.put('/usuario', validarRequisicao(usuarioSchema), editarUsuario);
-rotas.get('/usuario', detalharUsuario)
+rotas.put("/usuario", validarRequisicao(usuarioSchema), editarUsuario);
+rotas.get("/usuario", detalharUsuario);
 
+rotas.post("/produto");
+rotas.put("/produto/:id");
+rotas.get("/produto");
+rotas.get("/produto/:id");
+rotas.delete("produto/:id");
+
+rotas.post("/cliente");
+rotas.put("cliente/:id");
+rotas.get("/cliente");
+rotas.get("/cliente/:id");
 
 module.exports = rotas;
