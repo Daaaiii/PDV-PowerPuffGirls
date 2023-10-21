93f791bd9288627a21d7e06fff40a48f134c7e26
Julia Ajpert <jajpert@gmail.com>
2023-10-21 07:51:16 -0400

    feat: editar dados do produto

--- src/rotas.js
+++ src/rotas.js
@@ -12,7 +12,7 @@ const usuarioSchema = require("./validacoes/usuario");
 const loginSchema = require("./validacoes/login");
 const { cadastrarCliente } = require("./controladores/clientes");
 const clienteSchema = require("./validacoes/cliente");
-const { cadastrarProduto, listarProdutos, excluirProduto } = require("./controladores/produtos");
+const { cadastrarProduto, listarProdutos, excluirProduto, editarProduto } = require("./controladores/produtos");
 const produtoSchema = require("./validacoes/produto");
 
 const rotas = express();
@@ -28,8 +28,8 @@ rotas.put("/usuario", validarRequisicao(usuarioSchema), editarUsuario);
 rotas.get("/usuario", detalharUsuario);
 
 rotas.post("/produto", validarRequisicao(produtoSchema), cadastrarProduto);
-rotas.put("/produto/:id");
-rotas.get("/produto", listarProdutos);
+rotas.put("/produto/:id", editarProduto);
+rotas.get("/produto", validarRequisicao(produtoSchema), listarProdutos);
 rotas.get("/produto/:id");
 rotas.delete("produto/:id", excluirProduto);
 
