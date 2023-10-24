060a4bd328218531e5ad813d599c27f5f4428c51
Daiane Bolzan <dainha85@gmail.com>
2023-10-24 17:18:51 -0300

    fix: atualização da função de cadastrar produto

--- src/controladores/produtos.js
+++ src/controladores/produtos.js
@@ -2,51 +2,82 @@ const knex = require("../db");
 const {uploadImagem} = require("../servicos/upload");
 
 const cadastrarProduto = async (req, res) => {
-	const {descricao, quantidade_estoque, valor, categoria_id, produto_imagem} =
-		req.body;
-	const {originalname, mimetype, buffer} = req.file;
+	const {descricao, quantidade_estoque, valor, categoria_id} = req.body;
+	let produto_imagem = null;
 
-	try {
-		const categoriaExistente = await knex("categorias")
-			.where({id: categoria_id})
-			.first();
+	if (req.file) {
+		const {originalname, mimetype, buffer} = req.file;
 
-		if (!categoriaExistente) {
-			return res.status(404).json("A categoria informada não existe.");
+		try {
+			const categoriaExistente = await knex("categorias")
+				.where({id: categoria_id})
+				.first();
+
+			if (!categoriaExistente) {
+				return res.status(404).json("A categoria informada não existe.");
+			}
+
+			let produto = await knex("produtos")
+				.insert({
+					descricao,
+					quantidade_estoque,
+					valor,
+					categoria_id,
+					produto_imagem,
+				})
+				.returning("*");
+
+			if (!produto) {
+				return res.status(400).json("O produto não foi cadastrado.");
+			}
+
+			const id = produto[0].id;
+
+			const imagemDoProduto = await uploadImagem(
+				`produtos/${id}/${originalname}`,
+				buffer,
+				mimetype
+			);
+
+			produto = await knex("produtos")
+				.update({
+					produto_imagem: imagemDoProduto.url,
+				})
+				.where({id})
+				.returning("*");
+
+			return res.status(201).json(produto[0]);
+		} catch (error) {
+			return res.status(400).json(error.message);
 		}
-		let produto = await knex("produtos")
-			.insert({
-				descricao,
-				quantidade_estoque,
-				valor,
-				categoria_id,
-                produto_imagem
-			})
-			.returning("*");
+	} else {
+		try {
+			const categoriaExistente = await knex("categorias")
+				.where({id: categoria_id})
+				.first();
 
-		if (!produto) {
-			return res.status(400).json("O produto não foi cadastrado.");
+			if (!categoriaExistente) {
+				return res.status(404).json("A categoria informada não existe.");
+			}
+
+			let produto = await knex("produtos")
+				.insert({
+					descricao,
+					quantidade_estoque,
+					valor,
+					categoria_id,
+					produto_imagem,
+				})
+				.returning("*");
+
+			if (!produto) {
+				return res.status(400).json("O produto não foi cadastrado.");
+			}
+
+			return res.status(201).json(produto[0]);
+		} catch (error) {
+			return res.status(400).json(error.message);
 		}
-		const id = produto[0].id;
-     
-
-		const imagemDoProduto = await uploadImagem(
-			`produtos/${id}/${originalname}`,
-			buffer,
-			mimetype
-		);
-
-		produto =await knex("produtos")
-			.update({
-				produto_imagem: imagemDoProduto.url,
-			})
-			.where({id})
-			.returning("*");
-      
-	
-		return res.status(201).json(produto[0]);
-	} catch (error) {
-		return res.status(400).json(error.message);
 	}
 };
 
@@ -119,7 +150,6 @@ const detalharProduto = async (req, res) => {
 
 const editarProduto = async (req, res) => {
 	const {id} = req.params;
-	const {descricao, quantidade_estoque, valor, categoria_id} = req.body;
 
 	try {
 		const produtoExistente = await knex("produtos").where({id}).first();
@@ -137,10 +167,13 @@ const editarProduto = async (req, res) => {
 		}
 
 		const produto = await knex("produtos")
-			.update({descricao, quantidade_estoque, valor, categoria_id})
+			.update({
+				descricao,
+				quantidade_estoque,
+				valor,
+				categoria_id,
+			})
 			.where({id});
-
-		res.status(204).send();
 	} catch (error) {
 		return res.status(400).json(error.message);
 	}
--- src/validacoes/atualizarproduto.js
+++ src/validacoes/atualizarproduto.js
@@ -8,18 +8,18 @@ const atualizarProdutoSchema = joi.object({
 	quantidade_estoque: joi.number().integer().messages({
 		"any.required": "O campo quantidade do estoque é obrigatório",
 		"number.base": "A quantidade do estoque deve ser um número",
-        "number.integer": "A quantidade do estoque deve ser um número inteiro",
+		"number.integer": "A quantidade do estoque deve ser um número inteiro",
 	}),
-    valor: joi.number().messages({
+	valor: joi.number().messages({
 		"any.required": "O campo valor é obrigatório",
 		"number.base": "O valor deve ser um número",
 	}),
 	categoria_id: joi.number().integer().messages({
 		"any.required": "O campo de id da categoria é obrigatório",
 		"number.base": "O campo de id da categoria deve ser um número",
-        "number.integer": "O campo de id da categoria deve ser um número inteiro",
+		"number.integer": "O campo de id da categoria deve ser um número inteiro",
 	}),
-   
+	produto_imagem: joi.string(),
 });
 
-module.exports = atualizarProdutoSchema
+module.exports = atualizarProdutoSchema;
